964e82578c5438dac273beb0f2becb49bcbd60e5
jhx <jhx0x00@gmail.com>
2022-11-21 11:20:35 +0100

    moved js include, added email address, cleanup

--- website/index.php
+++ website/index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>IP Address</title>
+        <title>Your public IP Address</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Your public IP Address">
-        <meta name="author" content="jhx">
+        <meta name="author" content="jhx (jhx0x00@gmail.com)">
         
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <link href="style.css" rel="stylesheet">
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <link href="style.css" rel="stylesheet">
     </head>
     <body class="text-center">
         <div class="cover-container d-flex h-100 mx-auto flex-column">
@@ -24,6 +23,8 @@
                     <p>Created by <a href="https://jhx0.github.io/">jhx</a>.</p>
                 </div>
             </footer>
-        </div>   
+        </div>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     </body>
 </html>
